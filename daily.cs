069dea8ed65226e334ff430c1ee396620776b0d3
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-12-06 15:38:51 -0600

    12/6/22

---
+++ daily.cs
@@ -0,0 +1,30 @@
+// Complete the Add() method so that it will calculate the sum of two numbers given as arguments.
+// Overload it in order to do the same operation with double type values.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(10, 12));
+            Console.WriteLine(Add(1.5, 2.9));
+        }
+        //complete the method to sum
+        static int Add(int x, int y)
+        {
+            return x+y;
+        }
+        //overload it for double type
+        static double Add(double x, double y)
+        {
+            return x+y;
+        }
+    }
+}
