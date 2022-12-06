b58d77b96aa86fdc371e4e238993e421f6ef06f5
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-12-06 15:39:37 -0600

    12/6/22

--- daily.cs
+++ daily.cs
@@ -1,3 +1,4 @@
+// 12/6/22
 // Complete the Add() method so that it will calculate the sum of two numbers given as arguments.
 // Overload it in order to do the same operation with double type values.
 
