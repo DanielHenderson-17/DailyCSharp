e18d4df3f289c478388b34c7d57da8e124bac2e1
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2022-12-14 19:24:53 -0600

    12/14/2022

--- daily.cs
+++ daily.cs
@@ -126,3 +126,44 @@ namespace SoloLearn
         }
     }
 }
+
+
+// Recursion
+// Write a program to take N number as input and recursively calculate the sum of all numbers from 1 to N.
+// Sample Input
+// 5
+// Sample Output
+// 15
+// Explanation
+// 5+4+3+2+1 = 15.
+// Don't forget about base case.
+// 12/14/2022
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(Sum(number));
+        }
+        static int Sum(int num)
+        {
+            if (num == 1) {
+            	return 1;
+            }
+            else {
+            	return num + Sum(num - 1);
+            }
+        }
+    }
+}
+
